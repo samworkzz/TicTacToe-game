3b05821ef70eba68058f5865790b770e7c1212de
samworkzz <162050373+samworkzz@users.noreply.github.com>
2024-03-04 00:30:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+  let audiogameover= new Audio("gameover.mp3");
+  let audioturn= new Audio("ting.mp3");
+  let turn="X";
+  let gameover=false;
+
+  const changeTurn=()=>{
+    return turn==="X"? "O": "X"
+  }
+  const checkWin =() => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            gameover = true
+            audiogameover.play();
+            document.querySelector('.gameinfo').getElementsByTagName('img')[0].style.width="200px";
+        }
+    })
+}
+//   game logic
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if(boxtext.innerText=== ''){
+            boxtext.innerText=turn;
+            turn=changeTurn();
+            audioturn.play();
+            checkWin();
+            if(!gameover){
+                document.getElementsByClassName("info")[0].innerText= "turn for "+ turn;
+            }
+        }
+    })
+})
+
+// add onclick listener to playagain button
+reset.addEventListener('click', () =>{
+    let boxtexts= document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element=> {
+        element.innerText=""
+    });
+    turn= "X";
+    gameover=false;
+    document.getElementsByClassName("info")[0].innerText= "turn for "+ turn;
+    document.querySelector('.gameinfo').getElementsByTagName('img')[0].style.width="0px";
+    
+})
